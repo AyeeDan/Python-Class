0a00e8134d67be8e801a8336f2df2d77af8422db
Jackie-python <Jacquelinetan147@gmail.com>
2024-06-27 17:53:00 -0700

    Add files via upload

---
+++ Python Basic (Th: 5:30 - 7:00)/Homework/Jackie/main.py
@@ -0,0 +1,21 @@
+#TASK 1
+
+user_name = input("Enter your name here: ")
+print("Hello, ", user_name)
+
+score = 0
+print("The word score is equivalent to", score)
+
+#In programming, the term "variable" is something used to store a value.
+#For example, in my code the word score is a variable
+
+#TASK 2 
+
+num1 = int(input("Enter your age"))
+print("Hello ", user_name, "you are ", num1, "years old")
+
+color = input("What is your favorite color?")
+print("Wow, I really like", color, "too!")
+
+#The input function in programming allows you to ask the user to input some 
+#type of information into the program
