7e8ae7166c63ef4c2c127d2419f13f12a3997035
iiStarburstii <159740094+iiStarburstii@users.noreply.github.com>
2024-07-13 17:35:14 -0700

    Add files via upload

---
+++ Python Basic (Sat 5:30 - 7:00)/Homework/Madeline/Homework #12.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+print ("the date and time NOW!!!! is...:", datetime.now())
+print ("apparently import datetime doesn't work :(")
+print ("everything is imported now")
+
+import random
+
+totally_random_number_real = random.randint (1,10)
+print ("I put random.randomint (1,10), ran it, it gave me a syntax error, but the syntax error literally gave me the answer. It said, 'did you mean 'randint?' randint works.")
+print ("")
+print (totally_random_number_real)
+
+import math
+squarerootOMG = math.sqrt(56433324554)
+print (squarerootOMG)
+
+print ("It won't let me install matplotlib on IDLE, so I'll stop here. I will install matplotlib on Python 3.12")
+print ("I installed matplotlib but it says it doesn't recognize import as any command?")
+
+print ("")
+print ("...")
+print ("")
+print ("Uhh yeah, pls help")
