a85a99708bf0d48d82099bf3067149b05b1fc54a
Jackie-python <Jacquelinetan147@gmail.com>
2024-07-17 21:18:57 -0700

    Add files via upload

---
+++ week 5 hw.py
@@ -0,0 +1,55 @@
+#PART 1
+
+#Lists are mutable (means changeable), they store objects in an order and can be mixed with different values
+#Tuples are immutable (means unchangeable), once your enter the data the items and order cannot be changed
+#Indexing is listing or making the data into a specific order
+#The difference between lists and tuples in pythonn are that lists are mutable and tuples are not
+#In lists, indexing is creating a specific placement or order for the items
+
+#PART 2
+integer_list = ["9", "4", "18', 12"]
+integer_list.append("6")
+integer_list.remove("4")
+integer_list.insert(2, "5")
+
+print(integer_list)
+
+
+number1, number2 = numbers_tuple[0], numbers_tuple[1]
+number3, number4, number5 = numbers_tuple
+print(number1, number2)
+print(number3, number4, number5)
+
+
+#If you try to modify an element it will show an error
+
+
+#In Python, lists and tuples are both very useful data structures.
+#Lists have mutability, making them better for codes that are more flexiable
+#and dynamic. Tuples are immutable, making them ideal for fixed data where the
+#elements do not change. Choosing between lists and tuples depends on the
+#requirements for your code which includes wether your code should be modified
+#or wether the code is being stored. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
