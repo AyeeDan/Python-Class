e003cf4b57126a2084587f521c532fbfb609570c
Jackie-python <Jacquelinetan147@gmail.com>
2024-07-24 20:30:48 -0700

    Add files via upload

---
+++ week 6 hw.py
@@ -0,0 +1,16 @@
+#WEEK 6 HW
+name = 'Jacqueline Tan'
+print("Hello, my name is", name)
+
+name2 = name.split()[0]
+print(name2)
+question_mark = False
+
+text = input("Please enter a sentence regarding your favorite color:")
+print(text)
+print("The length of your sentence is:", len(text))
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.find("e"))
+print("Does your sentence have a question mark?", question_mark)
