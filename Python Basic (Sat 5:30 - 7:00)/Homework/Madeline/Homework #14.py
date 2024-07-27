b76533285478aa96ea6c565c8159a19213b632e2
iiStarburstii <159740094+iiStarburstii@users.noreply.github.com>
2024-07-27 13:56:15 -0700

    Add files via upload
    Homework for Class #14- (FILE HANDLING).

---
+++ Python Basic (Sat 5:30 - 7:00)/Homework/Madeline/Homework #14.py
@@ -0,0 +1,43 @@
+# Problem 1
+file = open("input.txt", "r")
+print (file.read())
+file.close()
+
+#i'm putting it down as owtput because i already have a file names output.txt from class
+woah = open("owtput.txt", "w")
+woah.write("\nOMG IT WORKSSSS")
+woah = open("owtput.txt", "r")
+print (woah.read())
+
+
+# Problem 2
+import csv
+dat = open ("data.csv", "a")
+dat.write("\nwoahahaihaa")
+dat = open ("data.csv", "r")
+print (dat.read())
+# I don't know if I did this correctly to be honest
+
+# Problem 3
+print ("I need help on this one. Did we learn this? I'll try my best to do it and fail badly")
+print (''' I tried but it doesn't work:
+wow = open ("picture.png", "r")
+print (wow.read())''')
+print ("")
+
+#Problem 4
+# I was unsure on this one.
+hmm = open ("data.json", "r")
+print (hmm.read())
+
+
+
+
+
+
+
+
+
+
+
+
